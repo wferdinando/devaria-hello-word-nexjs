22b72524e3f34d77f3738962885496acb0349447
Willyan Trautwein Ferdinando <willyantferdinando@gmail.com>
2022-02-09 22:08:50 -0300

    Projeto de Teste NextJS Devaria 2022

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -3,11 +3,13 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   name: string
+  idade: number
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  console.log(req.method);
+  res.status(200).json({ name: 'Willyan Ferdinando', idade: 32 });
 }
